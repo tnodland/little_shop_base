db59b83b3f4237c1e8ac4bc4bc2ababa11cee00b
ian douglas <ian.douglas@iandouglas.com>
2019-02-11 15:17:36 -0700

    db seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot_rails'
+
+include FactoryBot::Syntax::Methods
+
+OrderItem.destroy_all
+Order.destroy_all
+Item.destroy_all
+User.destroy_all
+
+admin = create(:admin)
+user = create(:user)
+merchant_1 = create(:merchant)
+
+merchant_2, merchant_3, merchant_4 = create_list(:merchant, 3)
+
+inactive_merchant_1 = create(:inactive_merchant)
+inactive_user_1 = create(:inactive_user)
+
+item_1 = create(:item, user: merchant_1)
+item_2 = create(:item, user: merchant_2)
+item_3 = create(:item, user: merchant_3)
+item_4 = create(:item, user: merchant_4)
+create_list(:item, 10, user: merchant_1)
+
+inactive_item_1 = create(:inactive_item, user: merchant_1)
+inactive_item_2 = create(:inactive_item, user: inactive_merchant_1)
+
+Random.new_seed
+rng = Random.new
+
+order = create(:completed_order, user: user)
+create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1, created_at: rng.rand(3).days.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_3, price: 3, quantity: 1, created_at: rng.rand(5).days.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_4, price: 4, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
+
+order = create(:order, user: user)
+create(:order_item, order: order, item: item_1, price: 1, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: rng.rand(23).days.ago, updated_at: rng.rand(23).hours.ago)
+
+order = create(:cancelled_order, user: user)
+create(:order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
+create(:order_item, order: order, item: item_3, price: 3, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
+
+order = create(:completed_order, user: user)
+create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1, created_at: rng.rand(4).days.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
+
+
+
+
+
+
+puts "Users created: #{User.count.to_i}"
+puts "Orders created: #{Order.count.to_i}"
+puts "Items created: #{Item.count.to_i}"
+puts "OrderItems created: #{OrderItem.count.to_i}"

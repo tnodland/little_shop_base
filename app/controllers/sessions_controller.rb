8ad7500fd4c6a9c6651beace036e798c6ef71fcc
ian douglas <ian.douglas@iandouglas.com>
2019-02-10 19:58:24 -0700

    closes #63, logs a user out and resets entire session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,6 +25,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    reset_session
     redirect_to root_path
   end
 end
---
+++ spec/features/user/logout_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Log out workflow' do
+  it 'destroys their session tracking for user' do
+    @user = create(:user, email:'user@gmail.com', password: 'password')
+
+    visit login_path
+
+    fill_in :email, with: @user.email
+    fill_in :password, with: 'password'
+    click_button 'Log in'
+
+    within '.navbar-right' do
+      click_link 'Log out'
+    end
+
+    expect(current_path).to eq(root_path)
+    within '.navbar-right' do
+      expect(page).to have_link('Log in')
+      expect(page).to_not have_link('Log out')
+    end
+  end
+end

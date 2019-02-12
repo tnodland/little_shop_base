149b33a4e7cd28d76497a46829a39e71e24f8d39
ian douglas <ian.douglas@iandouglas.com>
2019-02-11 17:15:13 -0700

    closes #59, removes a click test for index spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,4 +16,28 @@ RSpec.describe Item, type: :model do
     it { should have_many :order_items }
     it { should have_many(:orders).through(:order_items) }
   end
+
+  describe 'instance methods' do
+    describe '.avg_time_to_fulfill' do
+      scenario 'happy path, with orders' do
+        user = create(:user)
+        merchant = create(:merchant)
+        item = create(:item, user: merchant)
+        order_1 = create(:completed_order, user: user)
+        create(:fulfilled_order_item, order: order_1, item: item, quantity: 5, price: 2, created_at: 3.days.ago, updated_at: 1.day.ago)
+        order_2 = create(:completed_order, user: user)
+        create(:fulfilled_order_item, order: order_2, item: item, quantity: 5, price: 2, created_at: 1.days.ago, updated_at: 1.hour.ago)
+
+        actual = item.avg_time_to_fulfill[0..13]
+        expect(actual).to eq('1 day 11:30:00')
+      end
+      scenario 'sad path, no orders' do
+        user = create(:user)
+        merchant = create(:merchant)
+        item = create(:item, user: merchant)
+
+        expect(item.avg_time_to_fulfill).to eq('n/a')
+      end
+    end
+  end
 end

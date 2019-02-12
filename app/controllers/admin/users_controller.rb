7de5d724f184fd21632f869a8be9f32ad3ca7acf
ian douglas <ian.douglas@iandouglas.com>
2019-02-12 13:35:30 -0700

    closes #1, closes #2, adds rspec helper for logging in

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,4 +5,23 @@ class Admin::UsersController < Admin::BaseController
 
   def show
   end
+
+  def disable
+    user = User.find(params[:id])
+    set_active_flag(user, false)
+    redirect_to admin_users_path
+  end
+
+  def enable
+    user = User.find(params[:id])
+    set_active_flag(user, true)
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def set_active_flag(user, active_flag)
+    user.active = active_flag
+    user.save
+  end
 end

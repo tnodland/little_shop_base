3dd868aa930861ad028d1dda8158f5dda994aea1
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-02-12 19:06:30 -0700

    Admin can edit a user's profile, Resolves #53

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,4 +6,27 @@ class Admin::UsersController < Admin::BaseController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+    @form_path = [:admin, @user]
+    render :'users/edit'
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile has been updated"
+      redirect_to admin_user_path(@user)
+    # else
+    #   flash[:alert] = "That email address is already in use"
+    #   render :'users/edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :address, :city, :state, :zip, :password)
+  end
 end

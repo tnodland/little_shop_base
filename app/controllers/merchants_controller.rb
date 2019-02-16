b2c41074401e90bc769acbba81008ee3cfcf6b51
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-02-16 05:46:07 -0700

    WIP - merchant statistics

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,6 +6,7 @@ class MerchantsController < ApplicationController
     @top_three_merchants_by_revenue = @merchants.top_merchants_by_revenue(3)
     @top_three_merchants_by_fulfillment = @merchants.top_merchants_by_fulfillment_time(3)
     @bottom_three_merchants_by_fulfillment = @merchants.bottom_merchants_by_fulfillment_time(3)
+    @top_states_by_order_count = User.top_states_by_order_count(3)
   end
 
   def show

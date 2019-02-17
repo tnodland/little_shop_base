24f9aea0bef1b7010ee5ec0bfa9cd3918f675253
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-02-17 10:18:45 -0700

    Add orders by items shipped

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,17 @@ class Order < ApplicationRecord
   has_many :items, through: :order_items
 
   validates_presence_of :status
+
+  def self.top_orders_by_items_shipped(limit)
+    sorted_by_items_shipped.limit(limit)
+  end
+
+  def self.sorted_by_items_shipped
+    self.where(status: 1)
+        .joins(:order_items)
+        .where('order_items.fulfilled = true')
+        .group(:id)
+        .select('orders.id, sum(order_items.quantity) AS quantity')
+        .order('quantity DESC')
+  end
 end

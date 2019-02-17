c9b565a5d563f48fd6da7744b95f08245da2a4a2
ian douglas <ian.douglas@iandouglas.com>
2019-02-17 01:39:54 -0700

    profile orders list, closes #39

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,13 @@ class Order < ApplicationRecord
   has_many :items, through: :order_items
 
   validates_presence_of :status
+
+  def total_item_count
+    order_items.sum(:quantity)
+  end
+
+  def total_cost
+    oi = order_items.pluck("sum(quantity*price)")
+    oi.sum
+  end
 end

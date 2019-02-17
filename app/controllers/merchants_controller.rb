93dc4ee0b75fce493dd1e71f5bffbf75206ca966
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-02-17 09:50:30 -0700

    Add top cities by order count

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -7,6 +7,7 @@ class MerchantsController < ApplicationController
     @top_three_merchants_by_fulfillment = @merchants.top_merchants_by_fulfillment_time(3)
     @bottom_three_merchants_by_fulfillment = @merchants.bottom_merchants_by_fulfillment_time(3)
     @top_states_by_order_count = User.top_user_states_by_order_count(3)
+    @top_cities_by_order_count = User.top_user_cities_by_order_count(3)
   end
 
   def show

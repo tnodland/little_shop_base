a2df270f4ac3b34af084a6ed4fc7039fa820f977
ian douglas <ian.douglas@iandouglas.com>
2019-02-18 14:45:43 -0700

    closes #50, closes #26

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,4 +1,8 @@
 class Admin::MerchantsController < Admin::BaseController
   def show
+    @user = User.find(params[:id])
+    if @user.default?
+      redirect_to admin_user_path(@user)
+    end
   end
 end

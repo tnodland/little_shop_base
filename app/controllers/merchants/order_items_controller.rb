ea323a53559a32b96d99887e961b6d1e80afab32
ian douglas <ian.douglas@iandouglas.com>
2019-02-20 09:35:38 -0700

    closes #34

--- app/controllers/merchants/order_items_controller.rb
+++ app/controllers/merchants/order_items_controller.rb
@@ -4,6 +4,10 @@ class Merchants::OrderItemsController < ApplicationController
   def update
     oi = OrderItem.find(params[:id])
     oi.fulfill
+    if oi.order.order_items.where(fulfilled: false).empty?
+      oi.order.status = :completed
+      oi.order.save
+    end
     flash[:success] = "You have successfully fulfilled an item"
     redirect_to dashboard_order_path(oi.order)
   end
